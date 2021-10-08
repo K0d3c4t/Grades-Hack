88ffbe870a08e106fb3fde90552d3c19ac76edd2
K0d3c4t <89653781+K0d3c4t@users.noreply.github.com>
2021-10-08 11:12:45 -0500

    Update MainCode.js

--- MainCode.js
+++ MainCode.js
@@ -3,14 +3,24 @@ var Elements = document.getElementsByClassName("sg-font-larger-average");
 var i;
 for (i = 0; i < Elements.length; i++){
 if (i==0){
-Elements(i).textContent = "10"}
+Elements[i].textContent = "100"}
 if (i==1){
-Elements(i).textContent = "9"}
+Elements[i].textContent = "99"}
 if (i==2){
-Elements(i).textContent = "91"}
+Elements[i].textContent = "98"}
 if (i==3){
-Elements(i).textContent = "92"}
+Elements[i].textContent = "100"}
 if (i==4){
-Elements(i).textContent = "93"}
+Elements[i].textContent = " "}
+if (i==5){
+Elements[i].textContent = "96"}
+if (i==6){
+Elements[i].textContent = " "}
+if (i==7){
+Elements[i].textContent = " "}
+if (i==8){
+Elements[i].textContent = "100"}
+if (i==9){
+Elements[i].textContent = "97"}
 }}
 myFunction();

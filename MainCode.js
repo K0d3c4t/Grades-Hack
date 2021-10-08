dbffe3552a703eaf2cbf39b8860c0809a05b5692
K0d3c4t <89653781+K0d3c4t@users.noreply.github.com>
2021-10-08 08:09:59 -0500

    Create MainCode.js

---
+++ MainCode.js
@@ -0,0 +1,16 @@
+function myFunction(){ 
+var Elements = document.getElementsByClassName("sg-font-larger-average"); 
+var i;
+for (i = 0; i < Elements.length; i++){
+if (i==0){
+Elements.textContent = "10"}
+if (i==1){
+Elements.textContent = "9"}
+if (i==2){
+Elements.textContent = "91"}
+if (i==3){
+Elements.textContent = "92"}
+if (i==4){
+Elements.textContent = "93"}
+}
+myFunction();
